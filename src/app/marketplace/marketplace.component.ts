04a72b1566c2112ec28fe16ab28bd853362d6bc0
luke bertram and lena ha <luke@>
2018-01-25 08:43:27 -0800

    add edit and deltete functionality to whatever and stuff. I am good at git

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -16,11 +16,13 @@ import { FirebaseObjectObservable } from 'angularfire2/database';
 export class MarketplaceComponent implements OnInit {
 
   albums: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
   constructor(private router: Router, private albumService: AlbumService) { }
 
   ngOnInit() {
     this.albums = this.albumService.getAlbums();
+    // console.log(this.router.url);
   }
 
   goToDetailPage(clickedAlbum){
